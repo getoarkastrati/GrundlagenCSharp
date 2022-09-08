26d2d442d03addd8c7f1a17c33345f1e7b9bf8bc
Getoar Kastrati <getoar.k2@gso.schule.koeln>
2022-09-08 10:56:10 +0200

    Datentypen und Konventierung

---
+++ 3DatentypenKonventierung/Program.cs
@@ -0,0 +1,56 @@
+﻿//Aufgabe 1
+Console.WriteLine(short.MaxValue);
+Console.WriteLine(short.MinValue);
+/*
+16bit
+32767
+-32768
+*/
+
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+/*
+ 32bit
+2147483647
+-2147483648
+ */
+
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+/*
+64bit
+#9223372036854775807
+-9223372036854775808
+*/
+
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(float.MinValue);
+/*
+32bit
+ 3,4028235E+38
+-3,4028235E+38
+*/
+
+Console.WriteLine(double.MaxValue); 
+Console.WriteLine(double.MinValue);
+/*
+ 64bit
+1,7976931348623157E+308
+-1,7976931348623157E+308
+*/
+
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+/*
+ 128bit
+79228162514264337593543950335
+-79228162514264337593543950335
+*/
+
+
+
+//Aufgabe 2
+
+string vorname = "Getoar";
+Console.WriteLine(vorname);
+
