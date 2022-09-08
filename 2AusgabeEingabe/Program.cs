64d16076372a1e6eec3ed2a02ba3362061c56094
Getoar Kastrati <112069659+getoarkastrati@users.noreply.github.com>
2022-09-08 08:37:41 +0200

    Add files via upload

---
+++ 2AusgabeEingabe/Program.cs
@@ -0,0 +1,24 @@
+﻿//A1
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+//A2
+string vorname = "Getoar";
+string alter = "18";
+string nachname = "Kastrati";
+string stadt = "Koeln";
+string plz = "50969";
+string klasse = "bft22";
+
+Console.WriteLine(vorname);
+Console.WriteLine(nachname);
+Console.WriteLine(alter);
+Console.WriteLine(stadt);
+Console.WriteLine(plz);
+Console.WriteLine(klasse);
+//A3
+
+Console.WriteLine($"{vorname} ist {alter} Jahre alt und besucht die Klasse {klasse} des Georg-Simon-Ohm Berufskolleg");
