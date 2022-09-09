afc81e3eb7a7af306c971eca4d792c729556410b
Getoar Kastrati <getoar.k2@gso.schule.koeln>
2022-09-09 13:52:33 +0200

    Datentypen und Konventierung

--- 3DatentypenKonventierung/Program.cs
+++ 3DatentypenKonventierung/Program.cs
@@ -48,9 +48,28 @@ Console.WriteLine(decimal.MinValue);
 */
 
 
-
 //Aufgabe 2
 
 string vorname = "Getoar";
-Console.WriteLine(vorname);
+short alter = 18;
+bool frage = true;
+Console.WriteLine("Ihre Antworten:");
+Console.WriteLine($"1. {vorname}\n2. {alter}\n3. {frage}");
 
+
+//Aufgabe 3
+
+/*
+int Dollarzahl = 36;
+char Dollar = Dollarzahl;
+*/
+
+//Aufgabe 4
+
+Console.WriteLine(" ");
+Console.WriteLine("Geben Sie die erste Variable ein!");
+string v1_str = Console.ReadLine();
+
+short v1_short = Convert.ToInt16(v1_str);
+
+Console.WriteLine(v1_short);
